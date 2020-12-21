e32aeaf02b15d7942bbb2f425cf9b8157e97f87f
Ana Freitas <anaflaviafemme@gmail.com>
2020-12-21 18:02:57 -0300

    css/html

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,7 +2,7 @@ export const Login = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <h1>Login!</h1>
+      <h1>Faça o seu Login!</h1>
   `;
   return rootElement;
 };
