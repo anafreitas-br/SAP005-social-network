437d2ddd9ad9b5c403aabb66a1074da82d77128f
Edilene de Oliveira <edilenefern@gmail.com>
2020-12-21 17:53:00 -0300

    Segunda tentativa

--- src/services/index.spec.js
+++ src/services/index.spec.js
@@ -1,8 +1,8 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from './index';
+import { Cadastrar } from './index';
 
 describe('myFunction', () => {
   it('should be a function', () => {
-    expect(typeof myFunction).toBe('function');
+    expect(typeof Cadastrar).toBe('function');
   });
 });
