e81c221c91e3154b36c734e4559e1d0d2193e89a
Ana Freitas <anaflaviafemme@gmail.com>
2020-12-23 10:57:04 -0300

    testes e alterações

---
+++ src/pages/feed/index.js
@@ -0,0 +1,12 @@
+export const Feed = () => {
+    // Coloque sua página
+    
+    const rootElement = document.createElement('div');
+    rootElement.innerHTML = `
+    <button class="btn " id="btnLogout">
+    Sair
+  </button>
+    `;
+    return rootElement;
+  };
+  
