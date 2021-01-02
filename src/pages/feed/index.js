10aef58bf3e86b6fb38c84c4e34f26570eeeba60
edilene <edilenefern@gmail.com>
2021-01-02 15:05:20 -0300

    pull git

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -2,48 +2,51 @@ import { outLogin } from "../../services/index.js"
 
 
 export const Feed = () => {
-       const feedPage = document.createElement('div');
-       feedPage.innerHTML = `
-    <div class="profile">
-   <div class="imgUser">
-   </div>
-   </div>
-   <strong>Nome do Usuário</strong>
-        
-   <main class="mainFeed">
-     <div class="newPost">
-       <div class="infoUser">
-       
-        
-         <form action="" class="formPost">
-         <textarea name="textarea" placeholder="Vamos salvar a natureza?"></textarea>
-         <div class="iconsAndButton">
-         <div class="icons">
-
-         <button class="btn" id="btnEnjoy">curtir</button>
-         </div>
-         <button type="submit" class="btnSubmitForm">Publicar</button>
+      const feedPage = document.createElement('div');
+      feedPage.innerHTML = `
       
-         </div>
-         </form>
-       </div>
-     </div>
-     
-   </main>
-   <button class="btn " id="btnLogout">
-   Sair
- </button>
- <footer>@Rede Social Eco Green - Alunas laboratoria
- </footer>
-    
-    `;
-    const btnLogout = feedPage.querySelector('#btnLogout')
-    btnLogout.addEventListener('click', (event) => {
-      event.preventDefault();
-      outLogin();
-    });
-    
+  <div class="profile">
+  <div class="imgUser">
+  </div>
+  </div>
+  <strong>Nome do Usuário</strong>
+      
+  <main class="mainFeed">
+    <div class="newPost">
+      <div class="infoUser">
+      
+      
+        <form action="" class="formPost">
+        <textarea name="textarea" placeholder="Vamos salvar a natureza?"></textarea>
+        <div class="iconsAndButton">
+        <div class="icons">
 
-    return feedPage;
-  };
+        <button class="btn" id="btnEnjoy">curtir</button>
+        </div>
+        <button type="submit" class="btnSubmitForm">Publicar</button>
+    
+        </div>
+        </form>
+      </div>
+    </div>
+  </main>
+
+
+
+
+  <button class="btn " id="btnLogout">
+  Sair
+</button>
+<footer>@Rede Social Eco Green - Alunas laboratoria
+</footer>
   
+  `;
+  const btnLogout = feedPage.querySelector('#btnLogout')
+  btnLogout.addEventListener('click', (event) => {
+    event.preventDefault();
+    outLogin();
+  });
+  
+
+  return feedPage;
+};
