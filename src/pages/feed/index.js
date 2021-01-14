67bf707d42b651bce168752bf2792d528e72e023
Ana Freitas <anaflaviafemme@gmail.com>
2021-01-13 21:29:04 -0300

    postar na tela

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,4 +1,4 @@
-import { outLogin, savingData, postar } from '../../services/index.js';
+import { outLogin, savingData, getPosts } from '../../services/index.js';
 
 export const Feed = () => {
   const feedPage = document.createElement('div');
@@ -19,33 +19,52 @@ export const Feed = () => {
                   <button class="deletePost" onclick="deletePost()">Deletar</button>
                 </div>
               </form>
-            <ul id='posts'>
-            </ul>
             </div>
           </div>
-        </main>       
-        <main class="mainFeed">
-          <div class="newPost">
-
-            <div class="infoUser">
-
-              <form action="" class="formPost">
-              <input type="text" name="textarea" id="textPostUser" placeholder="Vamos salvar a natureza?"></input>
-              <div class="iconsAndButton">
-                <button type="button" class="btnLike">
-                <img class="like" src="./images/afirmativo.png" alt="curtir"> 
-                Curtir 
-                </button>
-              </div>
-              </form>
-            </div>
-          </div>
-        </main>
+        </main> 
+        <ul id='posts'>
+        </ul>      
+        
     <button class="btn " id="btnLogout">Sair</button>
   <footer>@Rede Social Eco Green - Alunas laboratoria
   </footer>
     
     `;
+
+  const createPostElement = (post) => {
+    const li = document.createElement('li');
+    const userPost = document.createElement('main');
+    userPost.classList.add('mainFeed');
+    userPost.innerHTML = `
+        <div class="newPost">
+
+          <div class="infoUser">
+
+            <form action="" class="formPost">
+            <input type="text" name="textarea" id="textPostUser" value = "${post.userPost}"></input>
+            <div class="iconsAndButton">
+              <button type="button" class="btnLike">
+              <img class="like" src="./images/afirmativo.png" alt="curtir"> 
+              Curtir 
+              </button>
+            </div>
+            </form>
+          </div>
+        </div>
+    `;
+    li.setAttribute('data-id', post.userUid);
+    li.appendChild(userPost);
+
+    return li;
+  };
+  const postsUl = feedPage.querySelector('#posts');
+  getPosts().then((snapshot) => {
+    snapshot.forEach((doc) => { 
+      postsUl.appendChild(createPostElement(doc.data()));
+    });
+  });
+  
+  const template = feedPage.querySelector('#posts');
 
   const btnLogout = feedPage.querySelector('#btnLogout');
   btnLogout.addEventListener('click', (event) => {
@@ -53,31 +72,31 @@ export const Feed = () => {
     outLogin();
   });
 
- 
-  
   const btnPost = feedPage.querySelector('.btnSubmitForm');
   const submitData = feedPage.querySelector('#textPost');
   btnPost.addEventListener('click', (event) => {
     event.preventDefault();
-    savingData(submitData.value);
-    postar();
+    const post = {
+      userPost: submitData.value,
+      like: 0,
+      userUid: firebase.auth().currentUser.email,
+    // data: ,
+    };
+    savingData(post);
+    postsUl.prepend(createPostElement(post));
   });
 
   const carregaPost = () => {
     const template = feedPage.querySelector('#posts');
-    postar(template);
-  }
-
-  btnPost.addEventListener('click', carregaPost())
+    getPosts(template);
+  };
+  btnPost.addEventListener('click', carregaPost());
 
   //  const deleteMsg = feedPage.querySelector('.deletePost');
   //  deleteMsg.addEventListener('click', (event) => {
   //   event.preventDefault();
   //    deleteNow();
   //  });
-
- 
-
 
   return feedPage;
 };
