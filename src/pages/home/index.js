a757a6c0addda8465c962007917d26159afe7ef6
edilene <edilenefern@gmail.com>
2021-01-14 09:14:34 -0300

    css

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,16 +2,16 @@ export const Home = () => {
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
      <div class="Initial">
-      <p> A sua rede SocioAmbiental, criada para conectar idéias que transformam o planeta! </p>
-      <button class="enter"><strong> Fazer Login </strong></button>
-      </br>
+      <p class='enter'> A sua rede SocioAmbiental, criada para conectar idéias que transformam o planeta!</p>
+     
       <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/dr5dueiANhI" 
       frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
       gyroscope; picture-in-picture" allowfullscreen></iframe>
+      
       </div>
       <footer>@Rede Social Eco Green - Alunas laboratoria
   </footer>
-</div>
+    </div>
   `;
   const enterSite = rootElement.querySelector('.enter');
   enterSite.addEventListener('click', (e) => {
@@ -20,3 +20,5 @@ export const Home = () => {
   });
   return rootElement;
 };
+
+// {/* <button class="enter"><strong> Fazer Login </strong></button> */}
